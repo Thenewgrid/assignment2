e6debacf222b10e8e11c25a34e84f068f7c7a22d
Thenewgrid <144764680+Thenewgrid@users.noreply.github.com>
2024-11-08 16:04:57 -0800

    script to download packages

--- project1/script.sh
+++ project1/script.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+# Script that will install packages from packages file.
+
+# Get every new line as a package.
+list=$(cat packages)
+
+# For loop to take each entry and installed it.
+for i in $list;
+do
+  # echo y | , this pipes y which means yes to the installation question after pacman -S $i(package name).
+  # https://stackoverflow.com/questions/7642674/how-do-i-script-a-yes-response-for-installing-programs
+  echo y | pacman -S $i
+done
