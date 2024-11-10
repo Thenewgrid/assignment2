cc528d27a1335281af2247875179c745f96e0269
Thenewgrid <wstone934@gmail.com>
2024-11-10 07:44:49 +0000

    Finalizing the script

--- project2/script.sh
+++ project2/script.sh
@@ -8,49 +8,67 @@ make_user () {
   shell="/bin/bash"
 
   echo ""
-  read -p "Which name do you want to use " name
-
-  echo ""
-  #read -p "What will be your password? " password
+  # Getting the username
+  read -p "Choose your username; " name
   
+  # Check if the username exists.
+  # https://www.baeldung.com/linux/user-exists-check
+  if grep -q "$name:" /etc/passwd;then
+    echo "Username, '$name' is in use."
+    make_user
+  else
+    echo "Complete"
+  fi
+  
+  # Getting the password 
+  echo ""
+  read -p "Choose your password; " password
+  echo "Complete"
+
+  # Getting user ID
   echo ""
   read -p "Which user id would you like? E.g 1000,1001..." uid
-  # Check if the user exists.
-  # https://www.baeldung.com/linux/user-exists-check
-  # cat /etc/passwd | grep $name
-
-  # Home directory.
-  #mkdir -p /home/$name
-
-  # copy the contents of the /etc/skel directory into the home directory.
-  #cp --recursive /etc/skel /home/$name
-
-  # add them to a primary group that matches there user name.
   
-  # grep -q "$uid" /etc/passwd && echo "taken bucko" || echo "fine"
-
   # https://collectingwisdom.com/bash-check-if-file-contains-string/
-  if grep ":$uid:" /etc/passwd; then
-    echo "Error: UID $uid is already in use." 
-    exit 1
+  # Check if user ID exists.
+  if grep -q ":$uid:" /etc/passwd; then
+    echo "UserID, '$uid' is in use."
+    make_user
   else
-    echo "UID $uid is available for use."
+    echo "Complete"
   fi
 
-  #echo "$name:x:1001" >> /etc/group
+  # Home directory.
+  mkdir -p /home/$name
 
-  # new user should have password. use passwd.
-
+  # copy the contents of the /etc/skel directory into the home directory.
+  cp --recursive /etc/skel /home/$name
+  
+  # Add user to a primary group that matches their name and user ID.
+  echo "$name:x:$uid:" >> /etc/group
 
   # Finish collecting informtaion, put it all together.
+  # Template
+  # arch:x:1000:1000:arch Cloud User:/home/arch:/bin/bash
+  
+  echo "$name:x:$uid:$uid:Regular added user hk:/home/$name:$shell" >> /etc/passwd
+  
+  # Setting the password for the user.
+  passwd $name
 
+  # change ownership so that the new user owns their stuff.
+  chown -R "$name:$name" /home/$name
+
+  # Making the user directory accessable to only the user.
+  # chmod -R g+w,o+w path/to/directory
+  chmod -R u+rwx,g-rwx,o-rwx /home/$name
 }
 
 # Check if the script is being run as root.
 # https://askubuntu.com/questions/15853/how-can-a-script-check-if-its-being-run-as-root
 if [[ $EUID -ne 0 ]]; then
   echo "The script must be run with root privileges."
-  # Return exit status 1 to show that the script was not run with root privileges.
+  # Return exit status 1 to show that the script was not run and instead failed.
   exit 1
 else
   # Calling the make_user function to start the program.
